802695b6d3233a92a6783d76c84066e96a9a6e32
Florian Stadler <florian@pulumi.com>
2024-11-26 10:53:21 +0100

    Re-enabled pod-info

--- app-of-apps/index.ts
+++ app-of-apps/index.ts
@@ -8,18 +8,18 @@ const stackNamespace = stackRef.getOutput("pulumiStackNamespace");
 const accessTokenSecret = stackRef.getOutput("accessTokenSecret");
 const kube = new k8s.Provider("lab-cluster", { kubeconfig });
 
-// const podInfo = new k8s.apiextensions.CustomResource("pod-info", {
-//     apiVersion: 'pulumi.com/v1',
-//     kind: 'Stack',
-//     metadata: {
-//         namespace: stackNamespace,
-//     },
-//     spec: {
-//         stack: `${org}/lab-pod-info`,
-//         projectRepo: "https://github.com/flostadler/homelab",
-//         repoDir: "apps/pod-info",
-//         commit: "main",
-//         accessTokenSecret: accessTokenSecret,
-//         destroyOnFinalize: true,
-//     }
-// }, { provider: kube });
+const podInfo = new k8s.apiextensions.CustomResource("pod-info", {
+    apiVersion: 'pulumi.com/v1',
+    kind: 'Stack',
+    metadata: {
+        namespace: stackNamespace,
+    },
+    spec: {
+        stack: `${org}/lab-pod-info`,
+        projectRepo: "https://github.com/flostadler/homelab",
+        repoDir: "apps/pod-info",
+        branch: "refs/heads/main",
+        accessTokenSecret: accessTokenSecret,
+        destroyOnFinalize: true,
+    }
+}, { provider: kube });
