0e516c329bc81bb23d9c05484a5e582d11104f39
Florian Stadler <florian@pulumi.com>
2024-11-26 11:16:04 +0100

    refactor stack-of-stacks

--- app-of-apps/index.ts
+++ app-of-apps/index.ts
@@ -1,25 +1,3 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as k8s from "@pulumi/kubernetes";
+import * as stack from "./stack";
 
-const org = pulumi.getOrganization();
-const stackRef = new pulumi.StackReference(`${org}/cluster/lab-cluster`)
-const kubeconfig = stackRef.getOutput("kubeconfig");
-const stackNamespace = stackRef.getOutput("pulumiStackNamespace");
-const accessTokenSecret = stackRef.getOutput("accessTokenSecret");
-const kube = new k8s.Provider("lab-cluster", { kubeconfig });
-
-const podInfo = new k8s.apiextensions.CustomResource("pod-info", {
-    apiVersion: 'pulumi.com/v1',
-    kind: 'Stack',
-    metadata: {
-        namespace: stackNamespace,
-    },
-    spec: {
-        stack: `${org}/lab-pod-info`,
-        projectRepo: "https://github.com/flostadler/homelab",
-        repoDir: "apps/pod-info",
-        branch: "refs/heads/main",
-        accessTokenSecret: accessTokenSecret,
-        destroyOnFinalize: true,
-    }
-}, { provider: kube });
+const podInfoS = stack.pulumiStack({ name: "pod-info", stack: "lab-pod-info", repoDir: "apps/pod-info" });
---
+++ app-of-apps/stack.ts
@@ -0,0 +1,35 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
+
+const org = pulumi.getOrganization();
+const stackRef = new pulumi.StackReference(`${org}/cluster/lab-cluster`)
+const kubeconfig = stackRef.getOutput("kubeconfig");
+const stackNamespace = stackRef.getOutput("pulumiStackNamespace");
+const accessTokenSecret = stackRef.getOutput("accessTokenSecret");
+const kube = new k8s.Provider("lab-cluster", { kubeconfig });
+
+export interface StackInfo {
+    name: string;
+    stack: string;
+    repoDir: string;
+    branch?: string;
+    commit?: string;
+}
+export function pulumiStack(stack: StackInfo) {
+    return new k8s.apiextensions.CustomResource(stack.name, {
+        apiVersion: 'pulumi.com/v1',
+        kind: 'Stack',
+        metadata: {
+            namespace: stackNamespace,
+        },
+        spec: {
+            stack: `${org}/${stack.stack}`,
+            projectRepo: "https://github.com/flostadler/homelab",
+            repoDir: stack.repoDir,
+            branch: stack.commit ? undefined : stack.branch ?? "refs/heads/main",
+            commit: stack.commit,
+            accessTokenSecret: accessTokenSecret,
+            destroyOnFinalize: true,
+        }
+    }, { provider: kube });
+}
