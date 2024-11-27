a6ada9da80140f2aa03bf03555149ba36a3aee59
Florian Stadler <florian@pulumi.com>
2024-11-27 15:49:06 +0100

    Remove pod info

--- app-of-apps/index.ts
+++ app-of-apps/index.ts
@@ -1,4 +1,4 @@
 import * as stack from "./stack";
 
-const podInfo = stack.pulumiStack({ name: "pod-info", stack: "lab-pod-info", repoDir: "apps/pod-info" });
+// const podInfo = stack.pulumiStack({ name: "pod-info", stack: "lab-pod-info", repoDir: "apps/pod-info" });
 const grafana = stack.pulumiStack({ name: "grafana", stack: "lab-grafana", repoDir: "apps/grafana" });
